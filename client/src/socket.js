dbbb7db4d5ec5f10367eb450837c31fc52019a0a
Divya Mishra <mishradivyagkp1999@gmail.com>
2025-02-16 16:52:10 +0530

    connected

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,8 +1,16 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://bookingapp-server-henna.vercel.app", {
+const socket = io("http://localhost:5000", {
   transports: ["websocket"], // Ensure WebSocket connection
   withCredentials: true, // Allow credentials (cookies, auth tokens)
+});
+
+socket.on("connect", () => {
+    console.log("Connected to WebSocket:", socket.id);
+});
+
+socket.on("connect_error", (err) => {
+    console.error("WebSocket Connection Error:", err);
 });
 
 export default socket;
